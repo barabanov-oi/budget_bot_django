d1a03ad65c10e2d8c76034190d86e0e3edd3d90d
Олег <barabanov.w-sites@yandex.ru>
2021-02-11 20:47:46 +0300

    В обработчик добавлен декоратор csrf

--- budgetbot/notifimanager/views.py
+++ budgetbot/notifimanager/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-
+@ensure_csrf_cookie
 def talkin_to_me_bruh(request):
     # please insert magic here
     print('Ok!')
