f5a4a1b0be2d1d7bbf866ac42fa6b887a8177541
Олег <barabanov.w-sites@yandex.ru>
2021-02-12 09:35:40 +0300

    Отладка вебхук, декодирование запроса

--- budgetbot/notifimanager/views.py
+++ budgetbot/notifimanager/views.py
@@ -6,8 +6,15 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def talkin_to_me_bruh(request):
+    if request.META['CONTENT_TYPE'] == 'application/json':
+        print("Получен JSON")
+        json_data = request.body.decode('utf-8')
+        print(json_data)
+
     # please insert magic here
     print('Ok!')
     print(request.GET)
     print(request.POST)
+    body = request.read()
+    print(body)
     return HttpResponse('OK')
