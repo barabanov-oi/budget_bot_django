507757d551113c4543eb5eec841433527e0be799
Олег <barabanov.w-sites@yandex.ru>
2021-02-16 11:32:31 +0300

    При от

--- budgetbot/notifimanager/services/botcommand/commands.py
+++ budgetbot/notifimanager/services/botcommand/commands.py
@@ -33,7 +33,7 @@ def yaddLogin(params: str, profile_id: int, msg_format=True):
     """
     return_data = {}
     profile, _ = Profile.objects.get_or_create(
-        chat_id=profile_id,
+        chat_id=int(profile_id),
     )
     # список для активированных логинов
     activate_logins = []
